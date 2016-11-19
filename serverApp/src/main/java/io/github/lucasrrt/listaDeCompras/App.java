82bad1d1de01d5efd06f73b8350e999318e5e88e
Bruno Vieira Costa <brunopsycon@gmail.com>
2016-11-19 14:23:32 -0200

    server connection to database

--- serverApp/src/main/java/io/github/lucasrrt/listaDeCompras/App.java
+++ serverApp/src/main/java/io/github/lucasrrt/listaDeCompras/App.java
@@ -1,8 +1,56 @@
 package io.github.lucasrrt.listaDeCompras;
 import static spark.Spark.*;
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 
 public class App {
-    public static void main(String[] args) {
-        get("/hello", (req, res) -> "Fala viado!");
-    }
+	public static void main(String[] args) {
+		get("/connect", (req, res) -> {
+			doStuff();
+			return "deu certo!";
+		});
+	}
+
+	private static void doStuff(){
+		System.out.println("-------- PostgreSQL "
+				+ "JDBC Connection Testing ------------");
+
+		try {
+
+			Class.forName("org.postgresql.Driver");
+
+		} catch (ClassNotFoundException e) {
+
+			System.out.println("Where is your PostgreSQL JDBC Driver? "
+					+ "Include in your library path!");
+			e.printStackTrace();
+			return;
+
+		}
+
+		System.out.println("PostgreSQL JDBC Driver Registered!");
+
+		Connection connection = null;
+
+		try {
+
+			connection = DriverManager.getConnection(
+					"jdbc:postgresql://127.0.0.1:5432/lista_de_compras","postgres","postgres");
+
+		} catch (SQLException e) {
+
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+
+		}
+
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Failed to make connection!");
+		}
+	}
 }
