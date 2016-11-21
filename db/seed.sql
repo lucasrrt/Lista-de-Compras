433130f80ea5991ed21b2561a2b3a5182c37c7a9
Lucas <lucasrrt@gmail.com>
2016-11-21 20:21:19 -0200

    Seed

--- db/seed.sql
+++ db/seed.sql
@@ -19,10 +19,10 @@ insert into produtos(nome,categoria_id) values ('detergente',4);
 
 
 insert into usuarios(login,senha) values ('brunovcosta','1234');
-insert into usuarios(login,senha) values ('lucasrrt','justinbieber');
+insert into usuarios(login,senha) values ('lucasrrt','1234');
 insert into usuarios(login,senha) values ('tnmarchiore','mariliaS2');
 insert into usuarios(login,senha) values ('eduardocandido6','chocolate');
-insert into usuarios(login,senha) values ('daviamorim','lucasrrt');
+insert into usuarios(login,senha) values ('a','a');
 
 insert into compras(preco,quantidade,mercado,data,usuario_id,produto_id) values
 (13,2,'pda','2016-12-23 22:12',1,10);
